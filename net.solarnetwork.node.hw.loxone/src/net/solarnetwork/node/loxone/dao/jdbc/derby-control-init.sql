17eddfac15c5578cdb20849c28b3507f3d6e983e
Matt Magoffin <matt@solarnetwork.net>
2016-09-22 07:07:36 +1200

    Make config_id the first column in constraints, because we want to easily query on all rows for the given config ID value.

--- net.solarnetwork.node.hw.loxone/src/net/solarnetwork/node/loxone/dao/jdbc/derby-control-init.sql
+++ net.solarnetwork.node.hw.loxone/src/net/solarnetwork/node/loxone/dao/jdbc/derby-control-init.sql
@@ -9,7 +9,7 @@ CREATE TABLE solarnode.loxone_control (
 	room_lo		BIGINT,
 	cat_hi		BIGINT,
 	cat_lo		BIGINT,
-	CONSTRAINT loxone_control_pk PRIMARY KEY (uuid_hi, uuid_lo, config_id)
+	CONSTRAINT loxone_control_pk PRIMARY KEY (config_id, uuid_hi, uuid_lo)
 );
 
 CREATE TABLE solarnode.loxone_control_state (
@@ -19,13 +19,13 @@ CREATE TABLE solarnode.loxone_control_state (
 	name		VARCHAR(255) NOT NULL,
 	event_hi	BIGINT NOT NULL,
 	event_lo	BIGINT NOT NULL,
-	CONSTRAINT loxone_control_state_control_fk FOREIGN KEY (uuid_hi, uuid_lo, config_id)
-		REFERENCES solarnode.loxone_control (uuid_hi, uuid_lo, config_id)
+	CONSTRAINT loxone_control_state_control_fk FOREIGN KEY (config_id, uuid_hi, uuid_lo)
+		REFERENCES solarnode.loxone_control (config_id, uuid_hi, uuid_lo)
 		ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX loxone_control_state_idx 
-ON solarnode.loxone_control_state (uuid_hi, uuid_lo, config_id, name);
+ON solarnode.loxone_control_state (config_id, uuid_hi, uuid_lo, name);
 
 INSERT INTO solarnode.sn_settings (skey, svalue) 
 VALUES ('solarnode.loxone_control.version', '1');
