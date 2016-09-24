4fbcf014b85f85c91a36c7f80d90e3d1ad904670
Matt Magoffin <matt@solarnetwork.net>
2016-09-25 10:20:44 +1300

    Tweak WebSocket setup to get working.

--- net.solarnetwork.node.setup.web.loxone/web/js/loxone-websocket.js
+++ net.solarnetwork.node.setup.web.loxone/web/js/loxone-websocket.js
@@ -2,13 +2,23 @@ $(document).ready(function() {
 	'use strict';
 	var csrf = SolarNode.csrfData;
 	var configId = $("meta[name='loxone-config-id']").attr("content");
-	var eventUrl = 'ws://' +document.location.host +SolarNode.context.path('/a/loxone-ws');
+	var eventUrl = 'ws://' +document.location.host +SolarNode.context.path('/loxone-ws');
 	var socket = new WebSocket(eventUrl);
-	var stompClient = Stomp.over(socket);
+	var client = Stomp.over(socket);
 	var headers = {};
 	headers[csrf.headerName] = csrf.token;
-	stompClient.connect(headers, function(frame) {
-		stompClient.send("/app/test", {configId:configId}, "Hi.");
-		//var subscription = stompClient.subscribe("/queue/test", callback);
+	client.connect(headers, function(frame) {
+		
+		var subscription = client.subscribe("/topic/test", function(message) {
+			if (message.body) {
+				console.log("got message with body %s", message.body)
+			} else {
+				console.log("got empty message");
+			}
+		});
+		
+		client.send("/app/test", {configId:configId}, "Hi.");
+	}, function (error) {
+        console.log("STOMP protocol error %s", error);
 	});
 });
